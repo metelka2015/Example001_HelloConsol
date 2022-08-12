c77390bab6c66e8e37e8af1f9d3edbb473ea6a5c
Elena <metelka2015@gmail.com>
2022-08-12 20:08:24 +0500

    Задача с семинара 1

---
+++ Seminar001/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат (число, умноженное на само себя).
+Console.WriteLine("Программа возведения чиса в квадрат");
+
+Console.WriteLine("Пожалуйста, введите целое число и нажмите enter");
+
+string? numberString = Console.ReadLine();
+
+int number = int.Parse(numberString);
+
+int sqrt = number * number; 
+
+Console.WriteLine("Квадрат числа " + number + " есть число " + sqrt);
