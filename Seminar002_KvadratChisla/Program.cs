4fbb471ddde80c02e4d4cdd318b80886da2ebb88
Elena <metelka2015@gmail.com>
2022-08-14 23:34:37 +0500

    Seminar example 2

---
+++ Seminar002_KvadratChisla/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго. 
+Console.WriteLine("Программа проверки квадрата числа");
+
+Console.WriteLine("Введите первое число ");
+
+string? number1 = Console.ReadLine();
+
+int a = int.Parse(number1);
+
+Console.WriteLine("Введите второе число");
+
+string? number2 = Console.ReadLine();
+
+int b = int.Parse(number2);
+
+if(a == b * b)
+{
+    Console.WriteLine("Yes");
+}
+else 
+    Console.WriteLine("No");
