948bbb06c2ec0a0e3daa6877b673da423338735f
Elena <metelka2015@gmail.com>
2022-08-15 18:43:23 +0500

    Seminar example 005

---
+++ Seminar005_LastNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿// 7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 
+// 456 ->   782 ->  918 -> 8
+Console.WriteLine("Программа вывода последнего числа");
+Console.WriteLine("Введите трехзначное число");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString);
+// int abc;
+if(number - number%1000 == 0)
+{
+    
+    if(number < 0) number = number*(-1);
+    if(number - number%100 != 0 && number - number%10!= 0)
+    {
+        int number3 = number%10;
+        Console.WriteLine(number3);
+    }
+    else
+    Console.WriteLine("Ошибка. Введите трехзначное число");
+
+}
+
+else
+    Console.WriteLine("Ошибка. Введите трехзначное число");
+
+
