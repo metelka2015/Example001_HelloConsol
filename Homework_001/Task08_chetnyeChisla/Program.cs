c59bd08b25ddcd980105b7892f4129260770d05d
Elena <metelka2015@gmail.com>
2022-08-16 13:17:28 +0500

    Task08

---
+++ Homework_001/Task08_chetnyeChisla/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Программа всех четных чисел");
+Console.WriteLine("Введите целое положительное число");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString);
+int index = 1;
+if(number >= 0) 
+{
+    while(index <= number)
+        {
+        if(index%2 == 0)
+        {
+            Console.WriteLine(index);
+            index++;
+        }
+        else
+          index++;  
+        }
+    
+}
+else
+{
+    Console.WriteLine("Ошибка. Введите целое положительное число");
+}
