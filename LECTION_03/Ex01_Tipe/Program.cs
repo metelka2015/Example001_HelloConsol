1b916004d655c7abd1d0959adb3a625db5a35c38
Elena <metelka2015@gmail.com>
2022-08-23 20:56:15 +0500

    lection311

--- LECTION_03/Ex01_Tipe/Program.cs
+++ LECTION_03/Ex01_Tipe/Program.cs
@@ -53,18 +53,46 @@ int Method3()
 
 //Вид 4 - методы, которые что-то принимаю и что-то возвращают.
 
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty; //String.Empty - пустая строка
+
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "w");
+// Console.WriteLine(res);
+
+
 string Method4(int count, string text)
 {
-    int i = 0;
+    
     string result = String.Empty; //String.Empty - пустая строка
-
-    while(i < count)
-    {
+    for(int i = 0; i < count;  i++)
+      {
         result = result + text;
-        i++;
     }
     return result;
 }
 
-// string res = Method4(10, "w");
+string res = Method4(10, "w");
 // Console.WriteLine(res);
+
+
+
+
+for(int i = 0; i <= 10; i++)
+{
+   for (int j = 2; j <= 10; j++)
+   {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+   } 
+   Console.WriteLine();
+}
+
