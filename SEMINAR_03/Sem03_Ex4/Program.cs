7c7db2da72893ac713e841b072f012c1efdc5f2a
Elena <metelka2015@gmail.com>
2022-08-26 21:25:31 +0500

    seminar3 ex4

---
+++ SEMINAR_03/Sem03_Ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Factorial(int digit)
+{
+
+    int result = 1;
+    for (int i = 1; i <= digit; i++)
+    {
+        result = i * result;
+    }
+    return result;
+
+}
+
+if (int.TryParse(Console.ReadLine()!, out var number))
+{
+    if (number >= 0)
+    {
+        int result = Factorial(number);
+        Console.WriteLine($"{number}! = {result}");
+    }
+    else Console.WriteLine("Введите положительное число");
+}
+else Console.WriteLine("Введено не число!");
+
