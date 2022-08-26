7278bba61ae6fbef5486e5f2265f2ecd3e459ddd
Elena <metelka2015@gmail.com>
2022-08-26 13:20:05 +0500

    seminar3 ex2

---
+++ SEMINAR_03/Sem03_Ex2_SumDigitals/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает число А 
+// и выдает сумму чисел от 1 до А. 
+// 7 -> 28, 4->10, 8 ->36
+
+int SumFrom1ToA(int a)
+{
+    int result = 0;
+
+    for (var i = 0; i <= a; i++)
+        result += i;
+    return result;
+}
+
+
+var number = int.Parse(Console.ReadLine()!);
+
+if (number <= 0)
+    return;
+var result = SumFrom1ToA(number);
+
+Console.WriteLine(result);
+
+
+
+
